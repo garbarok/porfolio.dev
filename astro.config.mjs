3368f4979e7366c6e69797a0f35bf89c7149d53b
Oscar Gallego <info@oscargallegoruiz.com>
2025-11-20 13:09:47 +0100

    Add global styles and import Tailwind CSS in Layout component
    - Imported global CSS file in Layout.astro
    - Created global.css file to include Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,22 @@ import tailwind from "@astrojs/tailwind"
 
 import robotsTxt from "astro-robots-txt"
 
+import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
+
+import mdx from "@astrojs/mdx";
+
+import markdoc from "@astrojs/markdoc";
+
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  integrations: [tailwind(), robotsTxt(), sitemap(), partytown(), mdx(), markdoc()],
+  site: 'https://porfolio.dev/',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
